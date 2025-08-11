c724f771914273986a04237abc117c8638047447
Wesley Paulo Ribeiro <wesley_paulo00@hotmail.com>
2025-08-11 18:57:01 -0300

    chore: add better auth setup

--- auth-schema.ts
+++ auth-schema.ts
@@ -52,16 +52,3 @@ export const account = pgTable("account", {
   createdAt: timestamp("created_at").notNull(),
   updatedAt: timestamp("updated_at").notNull(),
 });
-
-export const verification = pgTable("verification", {
-  id: text("id").primaryKey(),
-  identifier: text("identifier").notNull(),
-  value: text("value").notNull(),
-  expiresAt: timestamp("expires_at").notNull(),
-  createdAt: timestamp("created_at").$defaultFn(
-    () => /* @__PURE__ */ new Date(),
-  ),
-  updatedAt: timestamp("updated_at").$defaultFn(
-    () => /* @__PURE__ */ new Date(),
-  ),
-});
