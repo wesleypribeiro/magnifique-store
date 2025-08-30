bb81426115f08b8da230ef2d39fd322da0048ced
Wesley Paulo Ribeiro <wesley_paulo00@hotmail.com>
2025-08-30 16:51:51 -0300

    feat: add stripe webhook route

---
+++ src/app/api/stripe/webhook/route.ts
@@ -0,0 +1,34 @@
+import { db } from "@/db";
+import { orderTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export const POST = async (request: Request) => {
+  if (!process.env.NEXT_STRIPE_SECRET_KEY || !process.env.STRIPE_WEBHOOK_SECRET) {
+    return NextResponse.error();
+  }
+  const signature = request.headers.get("stripe-signature");
+  if (!signature) {
+    return NextResponse.error();
+  }
+  const text = await request.text();
+  const stripe = new Stripe(process.env.NEXT_STRIPE_SECRET_KEY);
+  const event = stripe.webhooks.constructEvent(
+    text,
+    signature,
+    process.env.STRIPE_WEBHOOK_SECRET
+  );
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object as Stripe.Checkout.Session;
+    const orderId = session.metadata?.orderId;
+    if (!orderId) {
+      return NextResponse.error();
+    }
+    await db.update(orderTable).set({
+      status: 'paid',
+    }).where(eq(orderTable.id, orderId));
+  }
+
+  return NextResponse.json({ received: true });
+}
